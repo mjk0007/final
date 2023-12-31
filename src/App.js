2a6c84bd5cb638aec6b69f74956d090d100c86e7
eisha01 <eishashafiq14@gmail.com>
2023-12-31 20:18:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Appbar from './components/NavBar';
+import HomePage from './components/Homepage';
+import Services from './components/services';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Upload from './components/Upload';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Appbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/upload' element={<Upload />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
